7e2ddedcec7873483064d5e31ea5bd1689707062
BorisMarko <borismarko552@gmail.com>
2024-03-02 19:05:19 +0200

    hp
    I improved the prototype of the health system, added a health bar. In distress, when creating enemies, I will use it to receive and deal damage

--- Assets/Scripts/Gameplay/Health/HealthData.cs
+++ Assets/Scripts/Gameplay/Health/HealthData.cs
@@ -6,15 +6,25 @@ public class HealthData : MonoBehaviour
     public event Action HealthChanged;
     public event Action Died;
 
-    [SerializeField] private int _currentHealth;
+    [SerializeField, Min(50)] private int _maxHealth;
+
+    private int _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
 
     public int CurrentHealth
     {
         get { return _currentHealth; }
-        private set
-        {
-            _currentHealth = value;
-        }
+        private set { _currentHealth = Mathf.Clamp(value, 0, _maxHealth); }
+    }
+
+    public int MaxHealth
+    {
+        get { return _maxHealth; }
+        private set { _maxHealth = value; }
     }
 
     public void AddHealth(int amount)
@@ -23,6 +33,22 @@ public class HealthData : MonoBehaviour
             return;
 
         CurrentHealth += amount;
+
         HealthChanged?.Invoke();
+    }
+
+    public void ReduceHealth(int amount)
+    {
+        if (amount <= 0)
+            return;
+
+        CurrentHealth -= amount;
+        HealthChanged?.Invoke();
+
+        if (CurrentHealth <= 0)
+        {
+            CurrentHealth = 0;
+            Died?.Invoke();
+        }
     }
 }
--- Assets/Scripts/Gameplay/Health/HealthView.cs
+++ Assets/Scripts/Gameplay/Health/HealthView.cs
@@ -1,28 +1,41 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthView : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _healthView;
     [SerializeField] private HealthData _health;
+    [SerializeField] private Slider _healthbar;
+
+    private void Start()
+    {
+        _healthbar.maxValue = _health.MaxHealth;
+        UpdateHealthView();
+    }
 
     private void OnEnable()
     {
         _health.HealthChanged += UpdateHealthView;
-    }
-
-    private void Start()
-    {
-        UpdateHealthView();
+        _health.Died += HandleDeath;
     }
 
     private void OnDisable()
     {
         _health.HealthChanged -= UpdateHealthView;
+        _health.Died -= HandleDeath;
     }
 
     private void UpdateHealthView()
     {
-        _healthView.text = _health.CurrentHealth.ToString();
+        _healthView.text = $"{_health.CurrentHealth}/{_health.MaxHealth}";
+        _healthbar.value = _health.CurrentHealth;
+
+    }
+
+    private void HandleDeath()
+    {
+        _healthbar.gameObject.SetActive(false);
+        _healthView.gameObject.SetActive(false);
     }
 }
